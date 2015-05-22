35cc386b742f73af5f813c6d32e50ee2943b095c
David Minnerly <dtreeLive@hotmail.com>
2015-05-22 10:29:51 -0400

    Create the region once for each Wave Road instance
    I keep making the mistake of generating things multiple times, when it
    only needs to be done once. One day I'll have this hammered into my head

--- src/Modules/WaveRoad/ClientWaveRoad.lua
+++ src/Modules/WaveRoad/ClientWaveRoad.lua
@@ -20,7 +20,21 @@ end
 function ClientWaveRoad.new(model)
   local self = setmetatable({}, ClientWaveRoad)
 
+  -- Padding to add around the region to make it bigger than the actual model.
+  --
+  -- This is set arbitrarily. The top of the Wave Road must extend much
+  -- farther than a player would normally be able to reach by jumping. This
+  -- is because gear is allowed. Players can make use of the Gravity Coil and
+  -- jump very high.
+  --
+  -- When the player is outside of the boundary the Wave Road disappears.
+  -- This should only happen if a player travels a greate distance upwards,
+  -- so people can still have fun with the Gravity Coil without having the
+  -- Wave Road disappear from under them.
+  local padding = Vector3.new(0, 24, 0)
+
   self.Model = model
+  self.Region = createRegionFromModel(model, padding)
 
   -- Keep the original parent in memory so that the Wave Road can be moved back
   -- later (Thus hiding it from view).
@@ -38,19 +52,6 @@ function ClientWaveRoad:Hide()
 end
 
 function ClientWaveRoad:InsideBoundary(part)
-  -- Padding to add around the region to make it bigger than the actual model.
-  --
-  -- This is set arbitrarily. The top of the Wave Road must extend much
-  -- farther than a player would normally be able to reach by jumping. This
-  -- is because gear is allowed. Players can make use of the Gravity Coil and
-  -- jump very high.
-  --
-  -- When the player is outside of the boundary the Wave Road disappears.
-  -- This should only happen if a player travels a greate distance upwards,
-  -- so people can still have fun with the Gravity Coil without having the
-  -- Wave Road disappear from under them.
-  local padding = Vector3.new(0, 24, 0)
-  local region = createRegionFromModel(self.Model, padding)
   return region:CastPart(part)
 end
 
