d29d425c5d1bba6e950dde98e7c4fd9b21b2e895
David Minnerly <dtreeLive@hotmail.com>
2016-09-16 18:10:39 -0400

    Permanently show the Sky Wave
    I realized while testing that seeing the Sky Wave up in the air while
    you're walking around is actually really cool. I want this to be the new
    default

--- src/game/ServerScriptService/Server.lua
+++ src/game/ServerScriptService/Server.lua
@@ -51,32 +51,17 @@ end)
 --------------------------------------------------------------------------------
 
 local function handleWaveWorld()
-  local skyWaveModel = replicatedStorage.SkyWave
+  local skyWaveModel = workspace.SkyWave
   local skyWave = WaveRoad.new(skyWaveModel)
   local waveStationUsed = remotes.getEvent("WaveStationUsed")
 
-  -- Always move players between cells *before* calling this function. The Sky
-  -- Wave's player list needs to be up-to-date before checking if it should be
-  -- shown or hidden.
-  local function setSkyWaveVisibility()
-    local skyWavePlayers = cells.SkyWave:GetPlayers()
-
-    if #skyWavePlayers == 1 then
-      skyWave:Show()
-    elseif #skyWavePlayers == 0 then
-      skyWave:Hide()
-    end
-  end
-
   local function enterSkyWave(player)
     world:EnterCell(cells.SkyWave, player)
-    setSkyWaveVisibility()
     skyWave:TransIn(player)
   end
 
   local function leaveSkyWave(player)
     world:EnterCell(cells.EchoRidge, player)
-    setSkyWaveVisibility()
   end
 
   local function detectOutOfBounds(player)
--- src/game/ServerScriptService/WaveWorld/WaveRoad.lua
+++ src/game/ServerScriptService/WaveWorld/WaveRoad.lua
@@ -22,29 +22,9 @@ function WaveRoad.new(model)
 
   local self = BaseModel.new(model)
 
-  -- Keep the original parent in memory so that the Wave Road can be moved back
-  -- later (Thus hiding it from view).
-  self.OrigParent = model.Parent
-
-  -- This condition will automatically determine if the model is visible or not.
-  --
-  -- The Workspace is the only container that renders objects, so if the model
-  -- is parented to it we know it can be seen, and vice versa.
-  self.Visible = model.Parent == workspace
-
   self.EntryPoint = model.TeleportPad
 
   return setmetatable(self, WaveRoad)
-end
-
-function WaveRoad:Show()
-  self.Model.Parent = workspace
-  self.Visible = true
-end
-
-function WaveRoad:Hide()
-  self.Model.Parent = self.OrigParent
-  self.Visible = false
 end
 
 function WaveRoad:TransIn(player)
