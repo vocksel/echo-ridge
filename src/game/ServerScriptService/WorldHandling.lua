38cca23c652b4793c8069cd363a9dbfbfe230b3f
David Minnerly <dtreeLive@hotmail.com>
2016-09-21 10:13:55 -0400

    Resolve #2
    Eventually we will need to reimplment this functionality, as changing
    the Skybox and maybe ambient lighting will become a common occourance
    soon enough.
    
    For the time being, none of this code is necessary, and the way we
    detect regions needs a rework anyway, so we'll revisit this at a
    later date.

--- src/game/ServerScriptService/WorldHandling.lua
+++ src/game/ServerScriptService/WorldHandling.lua
@@ -1,59 +1,20 @@
-local players = game:GetService("Players")
 local replicatedStorage = game:GetService("ReplicatedStorage")
-local serverScripts = game:GetService("ServerScriptService")
 
 local remotes = require(replicatedStorage.Events.Remotes)
-local WaveRoad = require(serverScripts.WaveWorld.WaveRoad)
-local World = require(serverScripts.Environment.World)
-local Cell = require(serverScripts.Environment.Cell)
 
-local cells = {
-  EchoRidge = Cell.new("EchoRidge"),
-  SkyWave = Cell.new("SkyWave")
-}
+local waveStationUsed = remotes.getEvent("WaveStationUsed")
+local skyWave = workspace.SkyWave
 
-local world = World.new(cells)
-
-players.PlayerAdded:connect(function(player)
-  players.PlayerRemoving:connect(function(leavingPlayer)
-    if player == leavingPlayer then
-      world:LeaveCurrentCell(player)
-    end
-  end)
-  -- Start the player off in Echo Ridge
-  world:EnterCell(cells.EchoRidge, player)
-end)
-
-local function handleWaveWorld()
-  local skyWaveModel = workspace.SkyWave
-  local skyWave = WaveRoad.new(skyWaveModel)
-  local waveStationUsed = remotes.getEvent("WaveStationUsed")
-
-  local function enterSkyWave(player)
-    world:EnterCell(cells.SkyWave, player)
-    skyWave:TransIn(player)
-  end
-
-  local function leaveSkyWave(player)
-    world:EnterCell(cells.EchoRidge, player)
-  end
-
-  local function detectOutOfBounds(player)
-    while true do
-      if not skyWave:PlayerWithinBoundary(player) then
-        leaveSkyWave(player)
-        break
-      end
-      wait(.25)
-    end
-  end
-
-  local function onWaveStationUsed(player)
-    enterSkyWave(player)
-    coroutine.wrap(detectOutOfBounds)(player)
-  end
-
-  waveStationUsed.OnServerEvent:connect(onWaveStationUsed)
+-- Moves `model` on top of `part`.
+--
+-- This is used to move a Player's Character ontop of the teleport pad on the
+-- Sky Wave.
+local function teleportOnTop(model, part)
+  local boundingBox = model:GetExtentsSize()
+  local offset = CFrame.new(0, boundingBox.y, 0)
+  model:SetPrimaryPartCFrame(part.CFrame * offset)
 end
 
-handleWaveWorld()
+waveStationUsed.OnServerEvent:connect(function(player)
+  teleportOnTop(player.Character, skyWave.TeleportPad)
+end)
