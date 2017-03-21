0bb6abffb54b708372d12e939b0a18a25e9e51f4
David Minnerly <dtreeLive@hotmail.com>
2017-03-21 18:12:07 -0400

    Remove LocalWorld and LocalCell classes
    These were used as a convinience so we didn't have to pass in the player for each operation.
    
    This ends up added unnecessary complication, along with API duplication as we have to keep LocalWorld and World, and LocalCell and Cell in sync.
    
    Having API duplication, along with more files to deal with out weights the benefit of not having to pass in the client. It doesn't even take many morelines to do so.

--- src/StarterPlayer/StarterPlayerScripts/CellHandler.local.lua
+++ src/StarterPlayer/StarterPlayerScripts/CellHandler.local.lua
@@ -1,15 +1,16 @@
+local lighting = game:GetService("Lighting")
 local players = game:GetService("Players")
 local replicatedStorage = game:GetService("ReplicatedStorage")
 
 local components = require(replicatedStorage.Services.ComponentService)
 local transmit = require(replicatedStorage.Events.Transmit)
-local LocalCell = require(replicatedStorage.Level.LocalCell)
-local LocalWorld = require(replicatedStorage.Level.LocalWorld)
+local Cell = require(replicatedStorage.Level.Cell)
+local World = require(replicatedStorage.Level.World)
 
 local client = players.LocalPlayer
 
 local function getNewCell(cellModel)
-  local cell = LocalCell.new(cellModel.Name)
+  local cell = Cell.new(cellModel.Name)
 
   -- HACK This isn't a very clean way of setting the TimeOfDay of the Cell. For
   -- right now we really just need a TimeOfDay changing implementation, but this
@@ -35,19 +36,21 @@ end
 local function setupWorld()
   local cellModels = components:GetByType("Cell")
   local cells = getCellsFromModels(cellModels)
-  local world = LocalWorld.new(cells, client)
+  local world = World.new(cells)
 
   -- Fired by StarterCharacterScripts.WarpListening. This lets us know when the
   -- client has been Warped to one of the Cell Models. From here we can perform
   -- the action to actually move them into the Cell.
   local warpedToCell = transmit.getLocalEvent("WarpedToCell")
 
-  world.CellEntered:connect(function(cell)
-    if cell.TimeOfDay then cell:UseTimeOfDay() end
+  world.CellEntered:connect(function(player, cell)
+    if client == player and cell.TimeOfDay then
+      lighting.TimeOfDay = cell.TimeOfDay
+    end
   end)
 
   -- Start off in Geo's Room.
-  world:EnterCell("GeosRoom")
+  world:EnterCell("GeosRoom", client)
 
   warpedToCell.Event:connect(function(cellModel)
     world:EnterCell(cellModel.Name)
